040b88184cb6bad6adb3b18840bafb56ae059667
marcosGO1970 <marcosgarciaoliver@yahoo.com.ar>
2022-07-23 12:34:23 -0300

    User Controller y Routes listo!

--- src/app.js
+++ src/app.js
@@ -10,9 +10,12 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-//Rutas: hay una sola a / que es el mainRouter.js
+//Rutas: hay una a / que es el mainRouter.js
+//y otra a /users que es al userRouter.js
 const mainRouter = require('./routes/mainRouter.js')
+const userRouter = require('./routes/userRouter.js')
 app.use('/', mainRouter)
+app.use('/user', userRouter)
 
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
