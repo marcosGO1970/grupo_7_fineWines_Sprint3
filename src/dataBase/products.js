44ec29e1e6be4ffb3ae41ea218eadf2d7518408a
Mlogascio1986 <matiaslogascio@gmail.com>
2022-07-24 18:59:14 -0300

    con la base de datos updeteada

--- src/dataBase/products.js
+++ src/dataBase/products.js
@@ -5,8 +5,9 @@ const products = [
         varietal: "Malbec",
         description: "etc",
         price: 2500,
-        descuento: 0.2 ,
-        image: "Angelica Zapata Malbec Alta X 750cc. (full).jpg"
+        descuento: 0 ,
+        image: "Angelica Zapata Malbec Alta X 750cc. (full).jpg",
+        visitado:true
     },
     {
         id: 2,
@@ -14,62 +15,69 @@ const products = [
         varietal: "Cara Sur Rosado",
         description: "Etc",
         price: 1800,
-        descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg"
+        descuento: 0,
+        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg",
+        visitado:true
     },
     {
         id: 3,
-        bodega: "Sebastian Zuccardi",
-        varietal: "Cara Sur Rosado",
+        bodega: "Salentein",
+        varietal: "Espumante Brut Rose",
         description: "Etc",
-        price: 1800,
+        price: 1600,
         descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg"     
+        image: "Champagne Salentein Espumante Brut Rose 750cc.jpg",
+        visitado:true 
     },    
     {
         id: 4,
-        bodega: "Sebastian Zuccardi",
-        varietal: "Cara Sur Rosado",
+        bodega: "DV Catena",
+        varietal: "Cabernet Malbec",
         description: "Etc",
-        price: 1800,
-        descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg" 
+        price: 1600,
+        descuento: 0.3,
+        image: "DV-CATENA-CABERNET SUAVIGNON-MALBEC.jpg",
+        visitado: false
     },
     {
         id: 5,
-        bodega: "Sebastian Zuccardi",
-        varietal: "Cara Sur Rosado",
+        bodega: "DV Catena",
+        varietal: "Malbec Malbec",
         description: "Etc",
-        price: 1800,
-        descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg" 
+        price: 3500,
+        descuento: 0,
+        image: "DV-CATENA-MALBEC-MALBEC.jpg",
+        visitado:true
     },
     {
         id: 6,
-        bodega: "Sebastian Zuccardi",
-        varietal: "Cara Sur Rosado",
+        bodega: "Escorihuela Gascon",
+        varietal: "Gran Reserva Chardonnay",
         description: "Etc",
-        price: 1800,
+        price: 2100,
         descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg" 
+        image: "Escorihuela Gascon Gran Reserva Chardonnay.jpg",
+        visitado: false
     },
     {
         id: 7,
-        bodega: "Sebastian Zuccardi",
-        varietal: "Cara Sur Rosado",
+        bodega: "Escorihuela Gascon",
+        varietal: "Gran Reserva Malbec",
         description: "Etc",
-        price: 1800,
+        price: 1500,
         descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg"     
+        image: "Escorihuela Gascon Gran Reserva Malbec.jpg",
+        visitado:false
     },    
     {
         id: 8,
-        bodega: "Sebastian Zuccardi",
-        varietal: "Cara Sur Rosado",
+        bodega: "Escorihuela Gascon",
+        varietal: "Malbec Full",
         description: "Etc",
-        price: 1800,
-        descuento: 0.2,
-        image: "Cara Sur Rosado - Sebastian Zuccardi - Vino Barreal San Juan.jpg" 
+        price: 3500,
+        descuento: 0,
+        image: "Escorihuela Gascón Malbec Full.jpg",
+        visitado: false
     }
 ]
 
